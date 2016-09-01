fb2894d0aca1cf5beb3e19ec59aaa09750e35980
RobertKraaijeveld <0890289@hr.nl>
2016-09-02 00:51:31 +0200

    Added windows 7 friendly version of the casanova compiler

---
+++ Windows7CompatibleCasanova/Samples/MyFirstSample/Assets/UnityCube.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityCube : MonoBehaviour
+{
+
+  public Color Color
+  {
+    get { return gameObject.GetComponent<Renderer>().material.color; }
+    set { gameObject.GetComponent<Renderer>().material.color = value; }
+  }
+
+  bool destroyed = false;
+  public Vector3 Scale
+  {
+    get
+    {
+      return gameObject.transform.localScale;
+    }
+    set
+    {
+      gameObject.transform.localScale = value;
+    }
+  }
+  public bool Destroyed
+  {
+    get { return destroyed; }
+    set
+    {
+      destroyed = value;
+      if (destroyed)
+        GameObject.Destroy(gameObject);
+    }
+  }
+
+  public Vector3 Position
+  {
+    get { return gameObject.transform.position; }
+    set { gameObject.transform.position = value; }
+  }
+
+  public static UnityCube Instantiate()
+  {
+    var _object = GameObject.Instantiate(Resources.Load("MyCube"), Random.insideUnitSphere, Quaternion.identity) as GameObject;
+    return _object.GetComponent<UnityCube>();
+  }
+
+  public static UnityCube Find()
+  {
+    return GameObject.Find("/MyCube").GetComponent<UnityCube>();
+  }
+
+}
+                                                                                                                                                         
