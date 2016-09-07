7b09d9ef986063cd2fd7ae90816922e5ed3d5413
Robert Kraaijeveld <0890289@hr.nl>
2016-09-07 10:32:06 +0200

    big5personality class added

---
+++ Assets/Big5Personality.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PersonalityProject {
+
+	public class Big5Personality : MonoBehaviour
+	{
+		public int ExtraversionScale;
+		public int EgoismScale;
+		public int ThoroughnessScale;
+		public int EmotionalStabilityScale;
+		public int OpennessScale;
+
+		public ArrayList toList()
+		{
+			return new ArrayList(){ this.ExtraversionScale}
+		}
+	}
+
+}            
--- Assets/UnityGoblin.cs
+++ Assets/UnityGoblin.cs
@@ -1,69 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UnityGoblin : MonoBehaviour
+namespace PersonalityProject 
 {
+	public class UnityGoblin : MonoBehaviour
+	{
+		Big5Personality big5Personality = new Big5Personality();
+		public ArrayList Big5Personality
+		{
+			get { 
+				return big5Personality; 
+				}
+			set { 
+				  ArrayList returnList = value;
+				}
+		}
 
-    public Color Color
-    {
-        get { return gameObject.GetComponent<Renderer>().material.color; }
-        set { gameObject.GetComponent<Renderer>().material.color = value; }
-    }
+		public Color Color
+		{
+			get { return gameObject.GetComponent<Renderer>().material.color; }
+			set { gameObject.GetComponent<Renderer>().material.color = value; }
+		}
 
-    public bool IsMovingLegs
-    {
-        get { return gameObject.GetComponent<Animation>(); }
-        set
-        {
-            if (value && !gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("walk")) {
+		public bool IsMovingLegs
+		{
+			get { return gameObject.GetComponent<Animation>(); }
+			set
+			{
+				if (value && !gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("walk")) {
                 
-                gameObject.GetComponent<Animator>().Play("run");
-            }
-        }
+					gameObject.GetComponent<Animator>().Play("run");
+				}
+			}
 
-    }
+		}
 
 
-    bool destroyed = false;
-    public Vector3 Scale
-    {
-        get
-        {
-            return gameObject.transform.localScale;
-        }
-        set
-        {
-            gameObject.transform.localScale = value;
-        }
-    }
-    public bool Destroyed
-    {
-        get { return destroyed; }
-        set
-        {
-            destroyed = value;
-            if (destroyed)
-                GameObject.Destroy(gameObject);
-        }
-    }
+		bool destroyed = false;
+		public Vector3 Scale
+		{
+			get
+			{
+				return gameObject.transform.localScale;
+			}
+			set
+			{
+				gameObject.transform.localScale = value;
+			}
+		}
 
-    public Vector3 Position
-    {
-        get { return gameObject.transform.position; }
-        set { gameObject.transform.position = value; }
-    }
+		public bool Destroyed
+		{
+			get { return destroyed; }
+			set
+			{
+				destroyed = value;
+				if (destroyed)
+					GameObject.Destroy(gameObject);
+			}
+		}
 
-    public static UnityGoblin Instantiate()
-    {
-        var _object = GameObject.Instantiate(Resources.Load("Goblin"), Random.insideUnitSphere, Quaternion.identity) as GameObject;
-        return _object.GetComponent<UnityGoblin>();
-    }
+		public Vector3 Position
+		{
+			get { return gameObject.transform.position; }
+			set { gameObject.transform.position = value; }
+		}
 
-    public static UnityGoblin Find()
-    {
-        return GameObject.Find("/Goblin").GetComponent<UnityGoblin>();
-    }
+		public static UnityGoblin Instantiate()
+		{
+			var _object = GameObject.Instantiate(Resources.Load("Goblin"), Random.insideUnitSphere, Quaternion.identity) as GameObject;
+			return _object.GetComponent<UnityGoblin>();
+		}
 
-}
+		public static UnityGoblin Find()
+		{
+			return GameObject.Find("/Goblin").GetComponent<UnityGoblin>();
+		}
 
-             
+	}
+     }                                                                      
